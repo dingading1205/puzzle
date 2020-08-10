302cc10720d646a6b2cf8b3f2ad17107e76e4981
dingading1205 <dingsh1205@gmail.com>
2020-08-10 12:06:49 +0200

    1st version

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
-import logo from "./logo.svg";
 import { Board } from "./Board";
 import "./App.css";
+import Photo from "./photo.jpg";
+import {Image} from "react-bootstrap"
 
-export const App = () => (
+export const App = (props) => {
+ 
+  return(
+
   <div className="app">
     <h1> Hello Puzzle</h1>
     <Board />
+    <Image src={Photo} thumbnail style={{
+      position: "fixed",
+      top: "80px",
+      left: "700px",
+      width: "200px",
+      height: "200px",
+      border: "5px solid white"
+    }}></Image>
   </div>
-);
+)
+
+};
--- src/Board.js
+++ src/Board.js
@@ -1,22 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { Square } from "./Square";
-import { StartGame } from "./StartGame";
 
 const PhotoLength = 600;
 
 export const Board = (props) => {
   const [size, setSize] = useState(3);
-  // const [NUM_PIECES, setNumPieces] = useState(9);
-  const [NUM_PIECES, setNUMPIECES] = useState(9);
-  const [PIECE_LENGTH, setPIECLENGTH] = useState(200);
-  useEffect(() => {
-    setNUMPIECES(size * size);
-    setPIECLENGTH(PhotoLength / size);
-    // setBoard(initRandonBoard);
-  }, [size]);
-  // var SIZE = parseInt(size);
+// const [NUM_PIECES, setNumPieces] = useState(9);
+  // const NUM_PIECES = size * size;
+  // const PIECE_LENGTH = PhotoLength / size
+  // const [NUM_PIECES, setNUMPIECES] = useState(9);
 
-  const oriBoard = [...Array(NUM_PIECES).keys()];
+  const [PIECE_LENGTH, setPIECLENGTH] = useState(200);
+
+  const oriBoard = [...Array(size * size).keys()];
 
   const squarePosition = (pos) => {
     const row = pos % size;
@@ -25,7 +21,7 @@ export const Board = (props) => {
   };
 
   const isBesideEmpty = (board, pos) => {
-    const emptySquare = board.indexOf(NUM_PIECES - 1);
+    const emptySquare = board.indexOf(size * size - 1);
     const imgPosition = squarePosition(pos);
     const emptyPosition = squarePosition(emptySquare);
     return (
@@ -37,32 +33,58 @@ export const Board = (props) => {
   };
 
   const changeBoard = (board, pos) => {
-    const newEmptySquare = board.indexOf(NUM_PIECES - 1);
+    const newEmptySquare = board.indexOf(size * size - 1);
     const index = board[pos];
-    board[pos] = NUM_PIECES - 1;
+    board[pos] = size * size - 1;
     board[newEmptySquare] = index;
 
     return board;
   };
 
   const initRandonBoard = () => {
-    let newBoard = [...Array(NUM_PIECES).keys()];
+    let newBoard = [...Array(size * size).keys()];
 
-    for (let i = 0; i <= 2; i++) {
+    for (let i = 0; i <= 5; i++) {
       const posBesideEmpty = newBoard.filter((pos) =>
         isBesideEmpty(newBoard, pos)
       );
       const posToMove =
         posBesideEmpty[Math.floor(Math.random() * posBesideEmpty.length)];
-      newBoard = changeBoard(newBoard, posToMove);
+        newBoard = changeBoard(newBoard, posToMove);
+
     }
     return newBoard;
+    // setHistory(newBoard);
   };
 
   const [board, setBoard] = useState(initRandonBoard);
   const [moves, setMoves] = useState(0);
   const [isWin, setIsWin] = useState(false);
   const [info, setInfo] = useState(`MOVES: ${moves}`);
+  // const [history, setHistory] = useState(board);
+  // console.log(board)
+
+  // const curretn = history[history.length-1]
+
+  //  const moveBack = history.map ((step,moves) => {
+  //    const desc = moves ?
+  //    "Back to Move : " + moves : "Back to Start";
+  //    return (
+  //      <li key={moves}>
+  //        <button onClick = {()=> this.jumpTo(moves)}>{desc}</button>
+  //      </li>
+  //    );
+  //  });
+  useEffect(() => {
+    setPIECLENGTH(PhotoLength / size);
+    setBoard(initRandonBoard);
+  },[size]);
+
+  const whetherSolved = () => {
+    if (board.toString() === oriBoard.toString()) {
+      setIsWin(true);
+    }
+  };
 
   const moveSquare = (square) => {
     const pos = board.indexOf(square);
@@ -71,15 +93,32 @@ export const Board = (props) => {
       setMoves(newMoves);
       const newBoard = changeBoard(board, pos);
       setBoard(newBoard);
+      // const addHistory = history.push(newBoard);
+      // setHistory(addHistory);
       whetherSolved();
     }
   };
 
-  const whetherSolved = () => {
-    if (board.toString() === oriBoard.toString()) {
-      setIsWin(true);
-    }
-  };
+  useEffect(() => {
+    isWin
+      ? setInfo(`YOU WIN WITH ${moves} MOVES !!!`)
+      : setInfo(`MOVES: ${moves}`);
+  }, [moves, isWin]);
+
+  // const newGame = () => {
+  //   if (document.getElementById("radio1").checked) {
+  //     setSize(2);
+  //   } else if (document.getElementById("radio2").checked) {
+  //     setSize(3);
+  //   } else if (document.getElementById("radio3").checked) {
+  //     setSize(4);
+  //   }
+  //   setIsWin(false);
+  //   setMoves(0);
+  //   setInfo(`MOVES: 0`);
+  //   var newBoard = initRandonBoard();
+  //   setBoard(newBoard);
+  // };
 
   useEffect(() => {
     isWin
@@ -103,67 +142,78 @@ export const Board = (props) => {
     setBoard(newBoard);
   };
 
-  const changeSize = (e) => {
-    const size1 = e.target.value;
-    // setSize(parseInt(size1));
-    // setTimeout(() => {
-    //   newGame();
-    // }, 100);
-
-    // midSize = parseInt(size);
-  };
 
   return (
     <div>
       <div
         className="gameinfo"
-        style={{
-          height: "50px",
-          width: `${size * PIECE_LENGTH + 30}px`,
-          margin: "10px",
-        }}
+        // style={{
+        //   width: `${PhotoLength + 30}px`,
+        //   margin: "10px",
+        //   display: "inline"
+        // }}
       >
-        <StartGame
-          onClick={() => {
-            newGame();
-          }}
-        />
-        {info} <br />
-        Puzzle Size
-        <div>
-          <input
-            type="radio"
-            id="radio1"
-            name="size"
-            value="2"
-            onClick={changeSize}
-          />{" "}
-          2*2
-          <input
-            type="radio"
-            id="radio2"
-            name="size"
-            value="3"
-            onClick={changeSize}
-            defaultChecked
-          />{" "}
-          3*3
-          <input
-            type="radio"
-            id="radio3"
-            name="size"
-            value="4"
-            onClick={changeSize}
-          />{" "}
-          4*4
+        <div className="size" style={{
+          paddingLeft: "1em"
+        }}>
+          <h5>Choose-Puzzle-Size</h5>
+          <div>
+            <input
+              type="radio"
+              id="radio1"
+              name="size"
+              value="2"
+              // onClick={changeSize}
+            />{" "}
+            2*2
+            <input
+              type="radio"
+              id="radio2"
+              name="size"
+              value="3"
+              // onClick={changeSize}
+              defaultChecked
+            />{" "}
+            3*3
+            <input
+              type="radio"
+              id="radio3"
+              name="size"
+              value="4"
+              // onClick={changeSize}
+            />{" "}
+            4*4
+          </div>
+          <div style= {{
+            position: "absolute",
+            top: "100px",
+            left : "400px",
+            color: "green"
+          
+        }}><b>{info}</b></div>
+          
         </div>
+        <br></br>
+        <button
+          onClick={newGame}
+          style={{
+            height: "30px",
+            marginLeft : "2em",
+            display: "inline"
+          }}
+        >
+         <b>New Game</b> 
+        </button>
+       
       </div>
+      <br></br>
+      {/* <GameSetting info={info} newGame={newGame}></GameSetting> */}
       <div
         className="gameBoard"
         style={{
           position: "relative",
-          width: `${size * PIECE_LENGTH + 30}px`,
-          height: `${size * PIECE_LENGTH + 30}px`,
+          width: `${PhotoLength + 30}px`,
+          height: `${PhotoLength + 30}px`,
         }}
       >
         {board.map((index, pos) => (
@@ -181,3 +231,76 @@ export const Board = (props) => {
     </div>
   );
 };
+
+// return (
+//   <div className="game">
+//     <div
+//       className="gameBoard"
+//       style={{
+//         position: "relative",
+//         height: `${size * PIECE_LENGTH + 30}px`,
+//         float: "left",
+//         width: `${size * PIECE_LENGTH + 30}px`,
+//       }}
+//     >
+//       {board.map((index, pos) => (
+//         <Square
+//           index={index}
+//           pos={pos}
+//           key={index}
+//           size={size}
+//           onClick={() => {
+//             moveSquare(index);
+//           }}
+//         />
+//       ))}
+//     </div>
+//     <div
+//       className="gameinfo"
+//       style={{
+//         // height: "50px",
+//         // width: `${size*PIECE_LENGTH + 30}px`,
+//         marginLeft: `${PhotoLength + 100}px`,
+//       }}
+//     >
+//       <div className="gamesetting">
+//         <form
+//           style={{
+//             padding: "0",
+//           }}
+//         >
+//           <h4>Puzzle Size :</h4>
+//           <input type="radio" id="radio1" name="size" value="2" /> 2*2
+//           <input
+//             type="radio"
+//             id="radio2"
+//             name="size"
+//             value="3"
+//             defaultChecked
+//           />{" "}
+//           3*3
+//           <input type="radio" id="radio3" name="size" value="4"/> 4*4
+//           <br></br>
+//           <br></br>
+//           <button
+//             onClick={() => {
+//               newGame();
+//             }}
+//             style={{
+//               height: "30px",
+//             }}
+//           >
+//             {" "}
+//             New Game
+//           </button>
+//         </form>
+//       </div>
+//       <div className="moveinfo">
+//         <h4>Information:</h4>
+//         {info}
+//       </div>
+//     </div>
+//     <div>{/* <ol>{moveBack}</ol> */}</div>
+//   </div>
+// );
+// };
