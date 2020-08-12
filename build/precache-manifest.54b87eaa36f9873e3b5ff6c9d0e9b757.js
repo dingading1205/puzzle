f8cb7a5bfd39c6744b0ca5c23706bc8aba363630
dingading1205 <dingsh1205@gmail.com>
2020-08-12 22:42:11 +0200

    Built for Github Page Version 0.3

---
+++ build/precache-manifest.54b87eaa36f9873e3b5ff6c9d0e9b757.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5b7826917d54e7eef3d6c948a652431",
+    "url": "/puzzle/index.html"
+  },
+  {
+    "revision": "8fb7db2752a46e36a561",
+    "url": "/puzzle/static/css/main.c745aed8.chunk.css"
+  },
+  {
+    "revision": "295dda04ff2fe048da9e",
+    "url": "/puzzle/static/js/2.3f708697.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/puzzle/static/js/2.3f708697.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fb7db2752a46e36a561",
+    "url": "/puzzle/static/js/main.713e11eb.chunk.js"
+  },
+  {
+    "revision": "9b52b77a332dd1bb826a",
+    "url": "/puzzle/static/js/runtime-main.3eecdee9.js"
+  },
+  {
+    "revision": "11e46fddadb7fa288ff7b8e6702e1b1a",
+    "url": "/puzzle/static/media/photo.11e46fdd.jpg"
+  }
+]);
